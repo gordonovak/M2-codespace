d9acf7568fbee2ac31a99fa43d79a55f8d5caebd
Gordie Novak <gordienovak@Gordies-MacBook-Pro.local>
2025-06-30 09:24:40 -0500

    fixing codespace again?

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,9 +6,14 @@ echo "========== STARTING SETUP =========="
 echo "Updating package lists..."
 sudo apt update
 
+sudo add-apt-repository ppa:canonical-server/server-backports
+sudo apt-get update
+sudo apt-get upgrade qemu-user-static
+
 echo "Installing dependencies..."
 sudo apt install -y software-properties-common curl
 
 echo "Adding Macaulay2 PPA..."
 sudo add-apt-repository ppa:macaulay2/macaulay2
+sude apt update
 sudo apt install macaulay2 
