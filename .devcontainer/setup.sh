71e3dfcf36226cb1d0bcdbe8f7ee30de651f14a6
Gordon Novak <161096797+gordonovak@users.noreply.github.com>
2025-06-30 16:33:21 +0000

    final devcontainer fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,8 +10,10 @@ echo "Installing dependencies..."
 sudo apt install -y software-properties-common curl
 
 echo "Adding Macaulay2 PPA..."
+
 echo "postfix postfix/main_mailer_type select No configuration" | sudo debconf-set-selections
 echo "postfix postfix/mailname string localhost" | sudo debconf-set-selections
+
 sudo add-apt-repository -y ppa:macaulay2/macaulay2
 sudo apt -y update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y macaulay2 
