e254af804b86baf1977a8563457842358251bd17
Gordie Novak <gordienovak@Gordies-MacBook-Pro.local>
2025-06-30 09:46:43 -0500

    please be done

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,12 +4,12 @@ set -e  # Stop on error
 echo "========== STARTING SETUP =========="
 
 echo "Updating package lists..."
-sudo apt update
+sudo apt -y update
 
 echo "Installing dependencies..."
 sudo apt install -y software-properties-common curl
 
 echo "Adding Macaulay2 PPA..."
 sudo add-apt-repository -y ppa:macaulay2/macaulay2
-sudo apt update
-sudo apt install macaulay2 
+sudo apt -y update
+sudo apt -y install macaulay2 
