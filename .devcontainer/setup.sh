bb71d590673e55c96635488c27147b54eacb46ef
Gordie Novak <gordienovak@Gordies-MacBook-Pro.local>
2025-06-30 10:48:35 -0500

    overriding some changes

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,6 +10,8 @@ echo "Installing dependencies..."
 sudo apt install -y software-properties-common curl
 
 echo "Adding Macaulay2 PPA..."
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:macaulay2/macaulay2
+echo "postfix postfix/main_mailer_type select No configuration" | sudo debconf-set-selections
+echo "postfix postfix/mailname string localhost" | sudo debconf-set-selections
+sudo add-apt-repository -y ppa:macaulay2/macaulay2
 sudo apt -y update
 sudo DEBIAN_FRONTEND=noninteractive apt -y install macaulay2 
